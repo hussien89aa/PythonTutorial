93ce5e3d0ed3a2e99e3ac24a1bbc998b47a2d764
mukul vashisht <mukulsharma1026@gmail.com>
2018-10-03 00:58:58 +0530

    add some comment to make it readable for beginner

--- loops_nested_loop.py
+++ loops_nested_loop.py
@@ -1,16 +1,16 @@
 
 
 
-
+#main function starts 
 def main():
     i=0
-    while(i<5):
+    while(i<5):  #outer while loop
         print("i={}".format(i))
-        i+=1
+        i+=1     #increment value of i by 1 i.e i = i+1      
         j=5
-        while(j>i):
-            print("j={}".format(j))
-            j-=1
+        while(j>i):  #inner while loop
+            print("j={}".format(j))     #format() funtion used for format the string in python
+            j-=1     #decrementing value of j by 1 i.e j=j-1       
 
 
 
