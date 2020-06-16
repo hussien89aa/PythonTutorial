d76dbdf314bf7e3c52e87a5c7dc58ff80daab252
sv-shiansh <sv_shiansh@outlook.com>
2020-06-17 02:40:35 +0530

    Converted the code for user defined inputs

--- function.py
+++ function.py
@@ -9,7 +9,7 @@ def main():
     n1 = int(input("ENTER FIRST NUMBER"))
     n2 = int(input("ENTER SECOND NUMBER"))
     result= SumNumbers(n1,n2)
-    print("z={}".format(result))
+    print("SUM = {}".format(result))
 
 
 
