3053526a20c9295d82f2e782df711529c6994fa9
Shivank Sharma <69038430+bluedefender070@users.noreply.github.com>
2020-09-30 21:28:37 +0530

    Update TuplesAndList.py

--- TuplesAndList.py
+++ TuplesAndList.py
@@ -3,17 +3,18 @@
 def main():
     #string
     Data="software engineer"
+    #String Slicing
     print(Data[0:5])
     #List
     Ages=[44,33,45,33,54]
     Ages.append(100)
-    Ages.insert(0,33)
+    Ages.insert(0,33)#insert with index value
     print(Ages)
     #Tuples
-    Ages=[44,33,45,33,54]
-    Ages.append(100)
-    Ages.insert(0,33)
-    print(Ages)
+    Ages_tp=(44,33,45,33,54)#A tupple is immutable i.e, it cannot be changed
+    Ages_tp.append(100)
+    Ages_tp.insert(0,33)
+    print(Ages_tp)
 
 
 
