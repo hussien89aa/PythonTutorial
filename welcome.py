8aff8d197e9a79858c8664239213123df791d15a
Abraham Guernsey <abrahamguernsey@gmail.com>
2020-10-01 18:40:54 -0400

    Add more welcome code

--- welcome.py
+++ welcome.py
@@ -1,2 +1,5 @@
-
-print('welcome to python')
+#This is a simple print statement
+print("welcome to python")
+#You can use both double quotes("") and single quotes('')
+#print("welcome to python'), This is incorrect, double quotes must only be used with double quotes and the same with single quotes
+#you can do print('"Do or do not there is no try" -Yoda') You can use double quotes inside of single quotes and vice-versa.
