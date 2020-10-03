665b262c7b32dac1bb625b9ddc5d34a873a7aa50
Vishal Gohel <60569628+VishalGohelishere@users.noreply.github.com>
2020-10-03 11:03:12 +0530

    Update conditional_if_elif.py

--- conditional_if_elif.py
+++ conditional_if_elif.py
@@ -1,15 +1,13 @@
 def main():
-    Age=input("enter your Age:")
-    if(int(Age)>=8 and int(Age)<=10):
+    Age=int(input("enter your Age:"))
+    if(Age>=8 and Age<=10):
         print("children")
-        print("children")
-        print("children")
-        print("children")
-    elif(int(Age)>=11 and int(Age)<=15):
+
+    elif(Age>=11 and Age<=15):
         print("kids")
-    elif(int(Age)>=16 and int(Age)<=18):
-        print("Tingers")
-    elif(int(Age)>=19 and int(Age)<=30):
+    elif(Age>=16 and Age<=18):
+        print("Teen-agers")
+    elif(Age>=19 and Age<=30):
         print("Young")
     else:
         print("Out of range")
