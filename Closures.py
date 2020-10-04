f8ba1f57eacefd0886ae2adbdcdd5c89d8f5a9ef
Rajani Kanta Panda <49396418+rkantapanda10@users.noreply.github.com>
2020-10-04 21:17:25 +0530

    Closures in Python
    Do coding in Python for Closures

---
+++ Closures.py
@@ -0,0 +1,12 @@
+def print_msg(msg):
+    # This is the outer enclosing function
+
+    def printer():
+        # This is the nested function
+        print(msg)
+
+    printer()
+
+# We execute the function
+# Output: Hello
+print_msg("Hello")
